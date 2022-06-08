a823628604e9f2cdc39d03142d83fab9983b4b0c
Max Page <maxkpage@gmail.com>
2022-06-08 16:23:35 -0500

    Add sesv2 changes

--- lib/aws/rails/mailer.rb
+++ lib/aws/rails/mailer.rb
@@ -1,4 +1,4 @@
-require 'aws-sdk-ses'
+require 'aws-sdk-sesv2'
 
 module Aws
   module Rails
@@ -19,21 +19,45 @@ module Aws
       # @param [Hash] options Passes along initialization options to
       #   [Aws::SES::Client.new](http://docs.aws.amazon.com/sdkforruby/api/Aws/SES/Client.html#initialize-instance_method).
       def initialize(options = {})
-        @client = SES::Client.new(options)
+        @client = SESV2::Client.new(options)
       end
 
       # Rails expects this method to exist, and to handle a Mail::Message object
       # correctly. Called during mail delivery.
       def deliver!(message)
-        send_opts = {}
-        send_opts[:raw_message] = {}
-        send_opts[:raw_message][:data] = message.to_s
+        # send_opts = {}
+        # send_opts[:raw_message] = {}
+        # send_opts[:raw_message][:data] = message.to_s
 
-        if message.respond_to?(:destinations)
-          send_opts[:destinations] = message.destinations
+        # if message.respond_to?(:destinations)
+        #   send_opts[:destinations] = message.destinations
+        # end
+
+        # @client.send_raw_email(send_opts).tap do |response|
+        #   message.header[:ses_message_id] = response.message_id
+        # end
+
+        send_opts = {}
+        send_opts[:content] = {}
+        send_opts[:content][:raw] = { data: message.to_s }
+
+        send_opts[:from_email_address] = message.from_address&.to_s
+
+        send_opts[:destination] = {}
+        send_opts[:destination][:to_addresses] = [*message.to]
+        send_opts[:destination][:cc_addresses] = [*message.cc]
+        send_opts[:destination][:bcc_addresses] = [*message.bcc]
+
+        send_opts[:configuration_set_name] = message.header['X-SES-CONFIGURATION-SET']&.yield_self do |field|
+          message.header.fields.delete(field).value
         end
 
-        @client.send_raw_email(send_opts).tap do |response|
+        send_opts[:list_management_options] = message.header['X-SES-LIST-MANAGEMENT-OPTIONS']&.yield_self do |field|
+          contact_list_name, topic_name = message.header.fields.delete(field).value.sub("topic=", "").split(";").map(&:strip)
+          {contact_list_name: contact_list_name, topic_name: topic_name}.compact
+        end
+
+        @client.send_email(send_opts).tap do |response|
           message.header[:ses_message_id] = response.message_id
         end
       end
